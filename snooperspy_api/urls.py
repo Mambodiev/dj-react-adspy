24afc2d53355ef24de478ebe10fa41adb833bea4
MamboDiev <mambodieu@gmail.com>
2022-09-03 15:02:34 +0300

    major change on product detail

--- snooperspy_api/urls.py
+++ snooperspy_api/urls.py
@@ -7,7 +7,6 @@ urlpatterns = [
     path('', ProductList.as_view(), name='listproduct'),
     path('product/<str:pk>/', ProductDetail.as_view(), name='detailproduct'),
     path('search/', ProductListDetailfilter.as_view(), name='searchproduct'),
-    # Product Admin URLs
     path('admin/create/', CreateProduct.as_view(), name='createproduct'),
     path('admin/edit/productdetail/<int:pk>/', AdminProductDetail.as_view(), name='admindetailproduct'),
     path('admin/edit/<int:pk>/', EditProduct.as_view(), name='editproduct'),
